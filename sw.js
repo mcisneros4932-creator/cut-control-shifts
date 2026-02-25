111de44a147b9730b3a61211d9b4d050c3a0e51e
mcisneros4932-creator <mcisneros4932@gmail.com>
2026-02-25 08:57:15 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "oilfield-cut-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e)=>{
+  e.waitUntil(
+    caches.open(CACHE).then((c)=>c.addAll(ASSETS)).then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then((r)=> r || fetch(e.request).then(res=>{
+      const copy = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, copy));
+      return res;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
